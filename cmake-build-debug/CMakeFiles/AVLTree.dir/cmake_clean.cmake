f6a59d7ba52180d3081e1bd9660e56e46a3f968e
Paul Menhart <menhart@hm.edu>
2017-12-18 08:31:37 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/AVLTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AVLTree.dir/library.cpp.obj"
+  "libAVLTree.pdb"
+  "libAVLTree.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVLTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef AVLTREE_LIBRARY_H
+#define AVLTREE_LIBRARY_H
+
+void hello();
+
+#endif
