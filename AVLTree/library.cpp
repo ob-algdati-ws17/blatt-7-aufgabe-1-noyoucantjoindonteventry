980c2c94ae5ac5eda7ac9a1aa9b5628d39f75b49
Paul Menhart <menhart@hm.edu>
2017-12-19 20:39:34 +0100

    added dummys for most methods

--- AVLTree/library.cpp
+++ AVLTree/library.cpp
@@ -2,6 +2,83 @@
 
 #include <iostream>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+AVLTree::~AVLTree() {
+    delete root;
+}
+
+const int *AVLTree::search(const int) const {
+    return nullptr;
+}
+
+const bool AVLTree::isEmpty() const {
+    return false;
+}
+
+int AVLTree::add(const int) {
+    return 0;
+}
+
+int AVLTree::remove(const int) {
+    return 0;
+}
+
+AVLTree::node *AVLTree::symmetricPrevious() const {
+    return nullptr;
+}
+
+vector<int> *AVLTree::preorder() const {
+    return nullptr;
+}
+
+vector<int> *AVLTree::inorder() const {
+    return nullptr;
+}
+
+vector<int> *AVLTree::postorder() const {
+    return nullptr;
+}
+
+AVLTree::node::node(const int key, AVLTree::node *prev, AVLTree::node *left, AVLTree::node *right)
+        : key(key), prev(prev), left(left), right(right) {
+}
+
+AVLTree::node::~node() {
+    delete left;
+    delete right;
+}
+
+void AVLTree::node::upIn() {
+
+}
+
+void AVLTree::node::upOut() {
+
+}
+
+bool AVLTree::node::search(const int) const {
+    return false;
+}
+
+void AVLTree::node::insert(const int) {
+
+}
+
+void AVLTree::node::remove(const int) {
+
+}
+
+vector<int> *AVLTree::node::preorder() const {
+    return nullptr;
+}
+
+vector<int> *AVLTree::node::inorder() const {
+    return nullptr;
+}
+
+vector<int> *AVLTree::node::postorder() const {
+    return nullptr;
+}
+
+AVLTree::node::node(const int key) : key(key) {
+
 }
--- AVLTree/library.h
+++ AVLTree/library.h
@@ -1,6 +1,58 @@
 #ifndef AVLTREE_LIBRARY_H
 #define AVLTREE_LIBRARY_H
 
-void hello();
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class AVLTree {
+private:
+    struct node {
+        const int key;
+        node *prev = nullptr;
+        node *left = nullptr;
+        node *right = nullptr;
+
+        node(const int);
+        node(const int, node *prev, node *left, node *right);
+        ~node();
+
+        bool search(const int) const;
+        void insert(const int);
+        void remove(const int);
+
+        vector<int> *preorder() const;  // (Hauptreihenfolge)
+        vector<int> *inorder() const;   // (Symmetrische Reihenfolge)
+        vector<int> *postorder() const; // (Nebenreihenfolge)
+
+        void upIn();
+
+        void upOut();
+    };
+
+    node *root = nullptr;
+
+    node *symmetricPrevious() const;
+
+public:
+    ~AVLTree();
+
+    const int *search(const int) const;
+
+    const bool isEmpty() const;
+
+    int add(const int);
+
+    int remove(const int);
+
+    vector<int> *preorder() const;
+
+    vector<int> *inorder() const;
+
+    vector<int> *postorder() const;
+
+    friend ostream &operator<<(ostream &, const AVLTree &);
+};
 
 #endif
